08acdf8340f744bfa41cf6f7871f8b3a93b55260
Saatvik <ssaatviks@gmail.com>
2024-12-28 20:28:19 +0530

    Dockerfile added

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173","http://localhost:4173"],
   methods: "GET,POST,PUT,DELETE",
   credentials: true,
 }));
