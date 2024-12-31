63261a75af2d25ab6b9ae4d974f7d2bb3f463ab2
Saatvik <ssaatviks@gmail.com>
2024-12-31 21:01:47 +0530

    working app

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,24 @@ const express = require("express");
 const router = express.Router();
 const blogController = require('../controllers/blog')
 
+router.get('/',(req,res) => {
+  res.send("Hello, Live check")
+})
+
+router.get('/api',(req,res) => {
+  res.send("Hello, api check")
+})
+
 router.get('/hello',(req,res) => {
   res.send("Hello, Health check")
 })
 
-router.post('/add-blog', blogController.addBlog)
-router.get('/get-blogs', blogController.getBlogs)
-router.post('/delete-blog', blogController.deleteBlog)
+router.get('/api/hello',(req,res) => {
+  res.send("Hello, api Health check")
+})
+
+router.post('/api/add-blog', blogController.addBlog)
+router.get('/api/get-blogs', blogController.getBlogs)
+router.post('/api/delete-blog', blogController.deleteBlog)
 
 module.exports = router;
